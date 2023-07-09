181dd5ef200c62b0cc9aa9c1bb52616bb69c9e7d
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-07-09 21:21:41 +0200

    Refactoring end-to-end test

--- e2e/tests/tests.sh
+++ e2e/tests/tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+URL="http://result:4000/" 
 current=0
 next=0
 new=0
@@ -11,41 +12,49 @@ done
 # add initial vote 
 curl -sS -X POST --data "vote=a" http://vote > /dev/null
 
-current=`phantomjs render.js "http://result:4000/" | grep -i vote | cut -d ">" -f 4 | cut -d " " -f1`
+current=$(phantomjs render.js $URL | grep -i vote | cut -d ">" -f 4 | cut -d " " -f1)
 # echo $current
 if [ -z "$current" ]; then current=1; else echo "Not NULL"; fi
 
-next=`echo "$(($current + 1))"`
+next=$((current + 1))
 
-  echo -e "\n\n-----------------"
-  echo -e "Current Votes Count: $current"
-  echo -e "-----------------\n"
+  echo -e """
+\n\n-----------------
+Current Votes Count: $current
+-----------------\n
+"""
 
 echo -e " I: Submitting one more vote...\n"
 
 curl -sS -X POST --data "vote=b" http://vote > /dev/null
 sleep 3
 
-new=`phantomjs render.js "http://result:4000/" | grep -i vote | cut -d ">" -f 4 | cut -d " " -f1`
+new=$(phantomjs render.js $URL | grep -i vote | cut -d ">" -f 4 | cut -d " " -f1)
 
 if [ -z "$new" ]; then new=$next; else echo "Not NULL"; fi
 
 
 
-  echo -e "\n\n-----------------"
-  echo -e "New Votes Count: $new"
-  echo -e "-----------------\n"
+  echo -e """
+\n\n-----------------
+New Votes Count: $new
+-----------------\n
+"""
 
 echo -e "I: Checking if votes tally......\n"
 
 if [ "$next" -eq "$new" ]; then
-  echo -e "\\e[42m------------"
-  echo -e "\\e[92mTests passed"
-  echo -e "\\e[42m------------"
+  echo -e """
+\\e[42m------------
+\\e[92mTests passed
+\\e[42m------------
+"""
   exit 0
 else
-  echo -e "\\e[41m------------"
-  echo -e "\\e[91mTests failed"
-  echo -e "\\e[41m------------"
+  echo -e """
+\\e[41m------------
+\\e[91mTests failed
+\\e[41m------------
+"""
   exit 1
 fi
