d1df8d5b42a133f8278737f4ad42183cc732f37b
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-07-09 21:04:41 +0200

    Refactoring integration test

--- vote/integration_test.sh
+++ vote/integration_test.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-cd integration
+cd integration || exit
 
 echo "I: Creating environment to run  integration tests..."
 
-docker-compose build
-docker-compose up -d
+docker compose build
+docker compose up -d
 
 
 echo "I: Launching Integration Test ..."
 
-docker-compose run --rm integration /test/test.sh
-
-if [ $? -eq 0 ]
+if docker compose run --rm integration /test/test.sh
 then
-  echo "---------------------------------------"
-  echo "INTEGRATION TESTS PASSED....."
-  echo "---------------------------------------"
-  docker-compose down
+  echo """
+---------------------------------------
+INTEGRATION TESTS PASSED.....
+---------------------------------------
+""" 
+  docker compose down
   cd ..
   exit 0
 else
-  echo "---------------------------------------"
-  echo "INTEGRATION TESTS FAILED....."
-  echo "---------------------------------------"
-  docker-compose down
+  echo """
+---------------------------------------
+INTEGRATION TESTS FAILED.....
+---------------------------------------
+""" 
+  docker compose down
   cd ..
   exit 1
 fi
